a334dd72886c17935e82c71622ca8ca9be97aac2
Douglas Souza <douglas_cts@hotmail.com>
2021-09-08 22:17:54 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuerior/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuerior.dsvendas.repositories;
+
+import com.devsuerior.dsvendas.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+
+}
